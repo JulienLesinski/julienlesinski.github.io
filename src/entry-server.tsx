7daaab6590b33fe6ce8680a3d8fe5ecc9f4b6e53
Julien Lesinski <lesinski.julien@hotmail.com>
2025-08-17 01:45:52 +0200

    Add CDN for Rockwell font.

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -9,6 +9,10 @@ export default createHandler(() => (
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
+          <link
+            href="https://fonts.cdnfonts.com/css/rockwell-nova"
+            rel="stylesheet"
+          />
           {assets}
         </head>
         <body>
